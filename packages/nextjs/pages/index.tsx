6383308ce2efd17f79434ca26e5158c4589707fe
DefiCody <ambergridpro@gmail.com>
2023-11-05 04:27:01 +0530

    m

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs  border-t-2 border-blue-400">
-               <IconBrandTelegram className="h-8 w-8 fill-primary"  color="#009fff"/>
+               <img src="/telegram.svg" className="h-8 w-8 fill-primary"  color="#009fff"/>
               <p>
                 Telegram Games, Sniper and Trading Bots,Token Scanning,Analysis , Notification and Stats Bots{" "}
                 <Link href="/debug" passHref className="link">
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs  border-b-2 border-blue-500">
-              <IconWebhook className="h-8 w-8 fill-primary" color="orange"/>
+            <img src="/trade-1.svg" className="h-8 w-8 fill-primary" color="orange"/>
               <p>
                 Token scanners and sniffer dapps with Honeypot checking, MemeCoin websites, clones of websites, Token cloning,{" "}
                 <Link href="/blockexplorer" passHref className="link">
@@ -56,23 +56,22 @@ const Home: NextPage = () => {
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs border-t-2 border-blue-400">
-              <RocketLaunchIcon className="h-8 w-8 fill-primary"  color="cyan"/>
+            <img src="/sniper-sight.svg" className="h-8 w-8 fill-primary"  color="cyan"/>
               <p>
                 Dex Sniper Bots, Antisweeper Bots, Presale sniper and HST Bots,Auto Trading bots with Take Profit, Stop Loss and Limit Orders{" "}
                 <Link href="/blockexplorer" passHref className="link">
                   for Uniswap V2 and V3
                 </Link>{" "}
-                Multichain
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs border-b-2 border-blue-500">
-              <IconBrandBinance  className="h-8 w-8 fill-primary"  color="orange"/>
+            <img src="/binance-logo.svg" className="h-8 w-8 fill-primary"  color="orange"/>
               <p>
                 Exchange Trading Bots, Copy & Auto Trading, Grid Trading, DCA Bots, with TP,SL and Limit Orders, both spot and futures. {" "}
                 <Link href="/blockexplorer" passHref className="link">
                   Perpetuals
                 </Link>{" "}
-                With Tradingview Connectivity.
+                With Tradingview .  
               </p>
             </div>
           </div>
